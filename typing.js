6c3f1f9ddb8d99e63468369156fcaaa58d4976c1
Josh Palay <joshpalay@gmail.com>
2013-10-23 01:56:13 -0400

    Redid typing stuff :D

--- typing.js
+++ typing.js
@@ -1,91 +1,22 @@
-(function ($) {
-  // writes the string
-  //
-  // @param jQuery $target
-  // @param String str
-  // @param Numeric cursor
-  // @param Numeric delay
-  // @param Function cb
-  // @return void
-  function typeString($target, str, cursor, delay, cb) {
-    $target.html(function (_, html) {
-      return html + str[cursor];
+$(function() {
+    var $cursor = $('<span />').html('&#9608;').addClass('blink');
+    typeString($('#logo'), 'THE HARVARD CRIMSON', 0, 100, $cursor, function() {
+        setTimeout( function() {
+            typeString($('#headline'), 'HACKING HARVARD', 0, 100, $cursor, 
+                function() {});
+        }, 500);
     });
-    if (cursor < str.length - 1) {
-      setTimeout(function () {
-        typeString($target, str, cursor + 1, delay, cb);
-      }, delay);
-    }
-    else {
-      cb();
-    }
-  }
-
-  // clears the string
-  //
-  // @param jQuery $target
-  // @param Numeric delay
-  // @param Function cb
-  // @return void
-  function deleteString($target, delay, cb) {
-    var length;
-
-    $target.html(function (_, html) {
-      length = html.length;
-      return html.substr(0, length - 1);
-    });
-
-    if (length > 1) {
-      setTimeout(function () {
-        deleteString($target, delay, cb);
-      }, delay);
-    }
-    else {
-      cb();
-    }
-  }
-
-  // jQuery hook
-  $.fn.extend({
-    teletype: function (opts) {
-      var settings = $.extend({}, $.teletype.defaults, opts);
-
-      return $(this).each(function () {
-        (function loop($tar, idx) {
-          // type
-          typeString($tar, settings.text[idx], 0, settings.delay, settings.cb);
-
-        }($(this), 0));
-      });
-    }
-  });
-
-  // plugin defaults  
-  $.extend({
-    teletype: {
-      defaults: {
-        delay: 100,
-        pause: 5000,
-        text: [],
-        cb: function() {}
-      }
-    }
-  });
-}(jQuery));
-
-$(document).ready(function() {
-  setTimeout(function() {
-    $('#logo').teletype({
-      text: [
-        'The Harvard Crimson'
-      ],
-      cb: function() {setTimeout($('#headline').teletype({text: ['Hacking Harvard'] }), 100000000)}
-    });
-  }, 1000);
-
-  $('#cursor').teletype({
-    text: ['_'],
-    delay: 0,
-    pause: 500
-  });
 });
+
+function typeString($target, str, pos, delay, $cursor, cb) {
+    $target.html(str.substring(0, pos + 1)).append($cursor);
+    if (pos < str.length - 1) {
+        setTimeout(function () {
+            typeString($target, str, pos + 1, delay, $cursor, cb);
+        }, delay);
+    }
+    else {
+        $target.append($cursor);
+        cb();
+    }
+}
