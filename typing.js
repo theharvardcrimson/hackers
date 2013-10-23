3394d8bd41072a76756378ad2a53abd22a68f6ad
Josh Palay <joshpalay@gmail.com>
2013-10-23 17:13:34 -0400

    changed things

--- typing.js
+++ typing.js
@@ -1,15 +1,31 @@
+var $cursor = $('<span />').html('&#9608;').addClass('blink cursor');
+
 $(function() {
-    var $cursor = $('<span />').html('&#9608;').addClass('blink');
-    typeString($('#logo'), 'THE HARVARD CRIMSON', 0, 100, $cursor, function() {
-        setTimeout( function() {
-            typeString($('#headline'), 'HACKING HARVARD', 0, 100, $cursor, 
-                function() {});
-        }, 500);
-    });
+    typeString($('#logo'), 'THE HARVARD CRIMSON', 0, 100, $cursor, type_headline);
 });
 
+function empty($cursor) {}
+
+function type_byline($cursor) {
+    $('.cursor').remove();
+    $('#byline').prepend($cursor.clone().css('color', 'black').removeClass('blink'));
+    console.log('prepended cursor copy to byline');
+    setTimeout(function() {
+        typeString($('#byline'), 'BY SAM WIENSTOCK AND DEV PATEL', 0, 100, 
+            $cursor, empty);
+    }, 500);
+}
+
+function type_headline($cursor) {
+    $('#headline').prepend($cursor.clone().css('color', 'black').removeClass('blink'));
+    setTimeout(function() {
+        typeString($('#headline'), 'HACKING HARVARD', 0, 100, 
+            $cursor, type_byline);
+    }, 500); 
+}
+
 function typeString($target, str, pos, delay, $cursor, cb) {
-    $target.html(str.substring(0, pos + 1)).append($cursor);
+    $target.append($cursor).find('.text').html(str.substring(0, pos + 1));
     if (pos < str.length - 1) {
         setTimeout(function () {
             typeString($target, str, pos + 1, delay, $cursor, cb);
@@ -17,6 +33,6 @@ function typeString($target, str, pos, delay, $cursor, cb) {
     }
     else {
         $target.append($cursor);
-        cb();
+        cb($cursor);
     }
 }
