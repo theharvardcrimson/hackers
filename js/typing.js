0f305d4327137be8ddc9468b34fa3c8f16de7686
Sam Fishman <sfishman@college.harvard.edu>
2013-10-24 07:43:33 -0400

    fixed more things

--- js/typing.js
+++ js/typing.js
@@ -1,40 +1,64 @@
 var $cursor = $('<span />').html('&#9608;').addClass('blink cursor');
+var $bin_txt;
 var HEADLINE = 'HACKING HARVARD';
-var BYLINE = 'BY SAMUEL Y. WEINSTOCK AND DEV A. PATEL';
+var BYLINE = 'BY DEV A. PATEL AND SAMUEL Y. WEINSTOCK';
 var SKIP = false;
-var FADE_TIME = 1000;
+var FINISHED = false;
+var FADE_TIME = 2000;
 
 $(document).ready(function() {
     var $statue = $('#statue');
-    typeString($('#logo'), 'THE HARVARD CRIMSON', 0, 100, $cursor, type_headline);
-    $(window).keypress(function() {
-        SKIP = true;
-    });
+    $bin_txt = $('#binary-text');
+    typeString($('#header'), 'THE HARVARD CRIMSON', 0, 100, $cursor, type_headline);
+    $(window).keydown(handle_click);
+    document.addEventListener('touchstart', handle_click);
 });
 
+function handle_click () {
+    if (FINISHED == true)
+        empty();
+    else
+        SKIP = true;
+}
+
 function empty($cursor) {
-    $('#skip').remove();
-    var $bin_txt = $('#binary-text');
     if (SKIP) {
-        $('#headline').fadeOut(FADE_TIME);
-        $('#byline').fadeOut(FADE_TIME);
+        $('#headline-wrapper').fadeOut(FADE_TIME);
+        $("#press-key").fadeOut(FADE_TIME);
         $('#content').fadeIn(FADE_TIME).promise().done(function() {
-            while ($bin_txt.height() < 2 * $(window).height()) {
-                console.log('gogo!');
+            while ($bin_txt.height() < $(window).height()) {
                 scroll_binary();
             }
             setInterval(scroll_binary, 50);
+            $("#header").addClass("animate-shadow");
+            load_disqus();
+            load_addthis();
         });
     }
     else {
-        setInterval(scroll_binary, 50);
-        setTimeout(function() {
-            $('#content').fadeIn(FADE_TIME);
-            $('#headline').fadeOut(FADE_TIME);
-            $('#byline').fadeOut(FADE_TIME);
-        }, 3800);
+        $('#headline-wrapper').fadeOut(FADE_TIME);
+        $("#press-key").fadeOut(FADE_TIME);
+        $('#content').fadeIn(FADE_TIME, function(){
+            $("#header").addClass("animate-shadow");
+            load_disqus();
+            load_addthis();
+        });
     }
-    $('#timeline').timelinexml({ src : 'timeline.xml' });
+    $('#timeline').timelinexml({ src : $('.timeline-html-wrap') });
+}
+
+function intro_done() {
+    if (!SKIP) {
+        FINISHED = true;
+        setInterval(scroll_binary, 50);
+        setTimeout(handle_click, 9000);
+        setTimeout(function() {
+            $("#press-key span").addClass("blink");
+        }, 4000);
+    }
+    else {
+        empty();
+    }
 }
 
 function type_byline($cursor) {
@@ -43,7 +67,7 @@ function type_byline($cursor) {
     if (!SKIP) {
         setTimeout(function() {
             typeString($('#byline'), BYLINE, 0, 100, 
-                $cursor, empty);
+                $cursor, intro_done);
         }, 500);
     }
     else {
@@ -82,35 +106,66 @@ function typeString($target, str, pos, delay, $cursor, cb) {
     }
 }
 
+function load_disqus() {
+    var disqus_shortname = 'thecrimson';
+    (function() {
+      var dsq = document.createElement('script');
+      dsq.type = 'text/javascript';
+      dsq.src = 'http://thecrimson.disqus.com/embed.js';
+      dsq.async = true;
+      (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0])
+          .appendChild(dsq);
+    })();
+}
+
+function load_addthis() {
+      addthis.layers({
+    'theme' : 'transparent',
+    'share' : {
+      'position' : 'right',
+      'numPreferredServices' : 5,
+      'services': 'facebook, twitter, email, print, more'
+    }
+  });
+}
+
 function scroll_binary() {
     // CONFIGURABLES:
     var balance = 0.5; // Balance between 1's and 0's
     var space_prob = 0.05; // Probability for spaces to appear
+    var penalty = 0.08; // Percent 1 or 0 is punished after each
+                        // repeated appearance
 
     // Generate line:
     var line = "";
-    var cut_off = (1 - space_prob) * balance;
+    var center = (1 - space_prob) * balance;
+    var cut_off = center;
     var rand = 0;
     for (var i = 0; i < 300; i++) {
         rand = Math.random();
         if (rand > (1 - space_prob)) {
             line += "&nbsp;";
-            cut_off = (1 - space_prob) * balance;
+            cut_off = center;
         }
-        if (rand >= cut_off) {
+        else if (rand >= cut_off) {
             line += "1";
-            if (cut_off < 0.5)
-                cut_off = (1 - space_prob) * balance;
-            cut_off += 0.1;
+            if (cut_off < center)
+                cut_off = center;
+            cut_off += penalty * (1 - space_prob);
         }
         else {
             line += "0";
-            if (cut_off > 0.5)
-                cut_off = (1 - space_prob) * balance;
-            cut_off -= 0.1;
+            if (cut_off > center)
+                cut_off = center;
+            cut_off -= penalty * (1 - space_prob);
         }
-
     }
 
-    $("#binary-text").append(line + "<br>");
+    // Pop hidden rows
+    // + 10 prevents yanking a line that's half-off prematurely
+    if ($bin_txt.height() > $(window).height() + 10) {
+        $bin_txt.children(":first").remove();
+    }
+
+    $bin_txt.append("<span>" + line + "<br></span>");
 }
